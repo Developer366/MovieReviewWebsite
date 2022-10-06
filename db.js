b0b368f0569ff0a92bc9dff8a4641bae27e32d21
Kamil Peza <>
2022-10-05 20:14:49 -0400

    heroku

--- db.js
+++ db.js
@@ -3,20 +3,31 @@
 
 var mysql = require('mysql');
 
-//local host test database for testing and building
+// local host test database for testing and building
 // const db = mysql.createConnection({//connect to mysql database
 // 	host: "127.0.0.1",
+// 	port: "3306",
 // 	user: "root",
 // 	password: "password1234",
 // 	database: "review_db",
 // });
 
+//AWS CONNECTION
+// const db = mysql.createConnection({//connect to mysql database
+// 	host: "aws-movie-review.cjnavmi6ggho.us-east-1.rds.amazonaws.com",
+// 	port: "3306",
+// 	user: "admin",
+// 	password: "mydatabse123",
+// 	database: "review_db",
+// });
+
 const db = mysql.createConnection({//connect to mysql database
-	host: "aws-movie-review.cjnavmi6ggho.us-east-1.rds.amazonaws.com",
+	// host: "abstract-gizmo-355616:us-central1:movie-review-db",
+	host: "34.173.117.227",
 	port: "3306",
-	user: "admin",
-	password: "mydatabse123",
-	database: "review_db",
+	user: "root",
+	password: "mydatabase123",
+	database: "movie-review",
 });
 
 
